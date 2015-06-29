5de2cb601438904710df017ea373775eb3f7e13c
zding5 <zding5@illinois.edu>
2015-06-29 17:59:43 -0500

    June.29 Rewriting well

--- CMedit.C
+++ CMedit.C
@@ -28,6 +28,11 @@
 #define  CMAP_YH    (CMAP_YMAX-CMAP_YMIN)
 
 
+#define  CMAPAPP_XMIN  data_x_min_for_cmap_
+#define  CMAPAPP_XMAX  data_x_max_for_cmap_
+#define  CMAPAPP_XW    (CMAPAPP_XMAX-CMAPAPP_XMIN)
+
+
 #define  DR_XMIN    (-1/16.f)
 #define  DR_XMAX    (1.0)
 #define  DR_XW      (DR_XMAX-DR_XMIN)
@@ -45,7 +50,7 @@
 
 #define  DISP_XMIN    data_x_min_for_display_
 #define  DISP_XMAX    data_x_max_for_display_
-#define  DISP_XW      (DT_XMAX-DT_XMIN)
+#define  DISP_XW      (DISP_XMAX-DISP_XMIN)
 
 
 #define  HDISP_YMIN  data_y_min_for_hist_display_
@@ -70,27 +75,53 @@ float CMedit::wy2dry( int wy ) {//pixel coordinate comes in as int
 
 //data to drawing
 float CMedit::dtx2drx( float dtx ) {
-  return (( dtx - DT_XMIN ) * ( DR_XFROM0 ) / DT_XW);
+  return (( dtx - DTHIST_XMIN ) * ( DR_XFROM0 ) / DTHIST_XW);
 }
 float CMedit::dty2dry( float dty ) {
-  return (( dty - DT_YMIN ) * ( DR_YFROM0 ) / DT_YH);
+  return (( dty - DTHIST_YMIN ) * ( DR_YFROM0 ) / DTHIST_YH);
 }
 
 //drawing to data
 float CMedit::drx2dtx( float drx ) {
-  return (DT_XMIN + (drx) * ( DT_XW ) / DR_XFROM0);
+  return (DTHIST_XMIN + (drx) * ( DTHIST_XW ) / DR_XFROM0);
+}
+
+//colormap apply to data
+float CMedit::cmapx2dtx( int cmapx ) {
+	return (CMAPAPP_XMIN + (cmapx - CMAPAPP_XMIN) * ( CMAPAPP_XW ) / DTHIST_XW)
 }
 
 float CMedit::dispx2dtx( int dispx ) {
-  return (DISP_XMIN + ( dispx - HIST_XMIN ) * ( DISP_XW ) / HIST_XW);
+  return (DTHIST_XMIN + ( dispx - DISP_XMIN ) * ( DTHIST_XW ) / DISP_XMIN);
 }
-// ***** Coordinates Conversion END
 float CMedit::h_dispy2dty( int h_dispy ) {
-	return 
+	return (HDISP_YMIN + ( h_dispy - HDISP_YMIN ) * ( DTHIST_YH ) / HDISP_YH);
 }
 float CMedit::c_dispy2dty( int c_dispy ) {
-
+	return (CDISP_YMIN + ( c_dispy - CDISP_YMIN ) * ( DTHIST_YH ) / CDISP_YH );
 }
+
+//data to colormap
+int CMedit::dtx2cmapx( float dtx ) {
+  if (dtx < CMAPAPP_XMIN) {
+    return CMAP_XMIN-1;
+  }
+  else if (dtx > CMAPAPP_XMAX) {
+    return CMAP_XMAX+1;
+  }
+  else {
+    int cmap_ind = (int) ( CMAP_XMIN + (dtx - CMAPAPP_XMIN) * ( CMAP_XW ) / CMAPAPP_XW);
+    return cmap_ind;
+  }
+}
+
+
+//Histogram y scaler. Will have more options later on I think
+float CMedit::hist_y_scaler( float dty ) {
+	return dty2dry(dty);
+} 
+
+// ***** Coordinates Conversion END
 
 CMedit::CMedit(int x, int y, int w, int h, const char *label)
 :Fl_Gl_Window(x,y,w,h,label) {
@@ -192,3 +223,49 @@ void CMedit::set_data_y_max_for_cmap_display(float val) {
 
 // **** Getters and Setters END
 
+
+// **** I/O Stuffs
+
+int cmap_fload( FILE *inf ) {
+	char* line;
+	size_t line_size;
+
+	float rgba[4], hsba[4], phsba[4];
+	static enum CMfield flds[4] = { HUE, SAT, BRIGHT, ALPHA };
+
+	int line_number;
+	char* buf1, buf2;
+
+	while(getline(&line, &line_size, inf) != -1) {
+		line_number++;
+		for(buf1 = line; *buf1 && isspace(*buf1); buf1++){
+			//skip potential spaces
+		}
+		if(*buf1 == '\0' || *buf1 == '\n'){
+			//skip potential empty lines
+			continue;
+		}
+	}
+}
+
+int cmap_fsave( FILE *outf ) {
+	return 0;
+}
+
+int hist_fload( FILE *inf ) {
+	return 0;
+}
+
+virtual void draw() {
+
+}
+virtual int handle(int ev) {
+
+}
+virtual void resize(int nx, int ny, int nw, int nh) {
+
+}
+
+
+
+// **** I/O Stuffs END
